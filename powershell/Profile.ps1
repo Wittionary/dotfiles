4a4c442c52f5a1210b7a68b3991a7631565a1790
Witt Allen <qwert59@gmail.com>
2020-10-15 22:47:34 -0500

    added powershell

---
+++ powershell/Profile.ps1
@@ -0,0 +1,55 @@
+<#
+This profile is going to get big and gross sooner than later.
+I should probably split up functions/aliases into another .ps1 and then import it as a module into this one.
+#>
+
+Clear-Host
+
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+
+function prompt {
+    
+    $realLASTEXITCODE = $LASTEXITCODE # This preserves our true lastexitcode
+    $Time = (Get-Date -Format HH:mm).ToString()
+    
+    # Window Title
+    if (Test-Administrator) {
+        # Use different username if elevated
+        $Host.UI.RawUI.WindowTitle = "(Elevated) $Time"
+    }
+    else {
+        $Host.UI.RawUI.WindowTitle = $Time
+    }
+    
+
+    $PromptTextColor = "Black"
+    $PromptBackgroundColor1 = "Yellow"
+    $PromptBackgroundColor2 = "Magenta"
+    $PromptBackgroundColor3 = "Blue"
+
+    # Username and hostname
+    Write-Host " $ENV:USERNAME" -NoNewline -ForegroundColor $PromptTextColor -BackgroundColor $PromptBackgroundColor1
+    # `u{2585} 
+    Write-Host " " -NoNewline -ForegroundColor $PromptTextColor -BackgroundColor $PromptBackgroundColor2
+    Write-Host "$ENV:COMPUTERNAME" -NoNewline -ForegroundColor $PromptTextColor -BackgroundColor $PromptBackgroundColor2
+
+    # Color for PS sessions
+    if ($null -ne $s) {
+        Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
+        Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
+        Write-Host ") " -NoNewline -ForegroundColor DarkGray
+    }
+
+    # Filepath
+    Write-Host " " -NoNewline -ForegroundColor $PromptBackgroundColor2 -BackgroundColor $PromptBackgroundColor3
+    Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\', '\\'), "~") -NoNewline -ForegroundColor $PromptTextColor -BackgroundColor $PromptBackgroundColor3
+    Write-Host "`u{25B6}" -NoNewline -ForegroundColor $PromptBackgroundColor3 -BackgroundColor $PromptTextColor
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+
+    return " "
+}

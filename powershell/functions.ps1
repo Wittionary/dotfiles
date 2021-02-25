dc0f2398b3ea1647de83522fa02631c69590f26a
Witt <Wittionary@users.noreply.github.com>
2021-02-25 10:15:45 -0600

    Added how-dare-you gif -> clipboard; this could probably live in espanso too/alternatively

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -134,3 +134,9 @@ function g {
         & git push
     }
 }
+
+# How Dare You gif
+function hdyg {
+    $GifURL = "https://media.giphy.com/media/U1aN4HTfJ2SmgB2BBK/giphy.gif"
+    $GifURL | Set-Clipboard
+}

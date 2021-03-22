60fa3e72a1edf97307a37d6a8869fcdd72c613a0
Witt <Wittionary@users.noreply.github.com>
2021-03-22 08:09:16 -0500

    Added comment

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -134,6 +134,7 @@ function hdyg {
 }
 
 # Fix Windows Terminal elevation prompt bug
+# Source: https://github.com/microsoft/terminal/issues/4217#issuecomment-712545620
 function Fix-WindowsTerminal { # Using an unapproved verb; come at me, bro.
     Add-AppxPackage -Register 'C:\Program Files\WindowsApps\Microsoft.WindowsTerminal_1.3.2651.0_x64__8wekyb3d8bbwe\AppxManifest.xml' -DisableDevelopmentMode
 }

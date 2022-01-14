6b2eb4d8d8f7991f1e37c83e8847eecbc2629173
Witt Allen <Wittionary@users.noreply.github.com>
2022-01-14 11:20:33 -0600

    Added 'tail' command

--- powershell/functions.ps1
+++ powershell/functions.ps1
@@ -319,6 +319,27 @@ function Process-DailyNote {
 
 # Terraform alias
 New-Alias -Name "tf" -Value "terraform.exe" -Description "Saves on 'terraform' keystrokes"
+# Tail alias
+function tail {
+    param (
+        [ValidateScript({Test-Path $_, "File not found at $_"})]
+        [Parameter(
+            Mandatory=$true,
+            Position = 0,
+            ValueFromPipeline = $true
+        )]
+        [String]
+        $Path,
+
+        [Parameter(
+            Position = 1
+        )]
+        [Int32]
+        $Count = 10
+    )
+    
+    Get-Content -Path $Path -Tail $Count
+}
 
 
 # Smart Insert/Delete

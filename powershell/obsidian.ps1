57f57802ade937d53239145308ca5ba4d4818746
Witt Allen <Wittionary@users.noreply.github.com>
2022-03-09 16:13:14 -0600

    Prepping for automating time entries

--- powershell/obsidian.ps1
+++ powershell/obsidian.ps1
@@ -21,6 +21,8 @@ function Calculate-TimeElapsed {
 
     )
     $CumulativeTime = $null
+    $ClockFormatPattern = "\d{1,2}:\d{1,2}\s?-\s?\d{1,2}:\d{1,2}"
+    $DurationFormatPattern = "\d{1,}\s?h([a-z\s]*\d{1,}\s?m[a-z]*)?"
 
     # Split into multiple sessions depending on the delimiter
     if (($RawSessions -match ",") -and ($RawSessions -match ";")) {
@@ -37,18 +39,23 @@ function Calculate-TimeElapsed {
     # Parse and add up each individual session
     foreach ($Session in $Sessions) {
         $Session = $Session.Trim()
-        # When there's a trailing delimiter, don't throw an error. Just ignore that empty "session"
-        if ($null -eq $Session -or $Session -eq "") {
-            break
+        if ($Session -match $ClockFormatPattern) {
+            # When there's a trailing delimiter, don't throw an error. Just ignore that empty "session"
+            if ($null -eq $Session -or $Session -eq "") {
+                break
+            }
+            $RawStartTime = $Session.Split("-")[0].Trim()
+            $RawEndTime = $Session.Split("-")[1].Trim()
+
+            $StartTime = Get-Date -Hour ($RawStartTime.Split(":")[0]) -Minute ($RawStartTime.Split(":")[1])
+            $EndTime = Get-Date -Hour ($RawEndTime.Split(":")[0]) -Minute ($RawEndTime.Split(":")[1])
+
+            $ElapsedSession = $EndTime - $StartTime
+            $CumulativeTime += $ElapsedSession
+        } elseif ($Session -match $DurationFormatPattern) {
+            $ElapsedSession = Calculate-TimeDuration -RawSession $Session
+            $CumulativeTime += $ElapsedSession
         }
-        $RawStartTime = $Session.Split("-")[0].Trim()
-        $RawEndTime = $Session.Split("-")[1].Trim()
-
-        $StartTime = Get-Date -Hour ($RawStartTime.Split(":")[0]) -Minute ($RawStartTime.Split(":")[1])
-        $EndTime = Get-Date -Hour ($RawEndTime.Split(":")[0]) -Minute ($RawEndTime.Split(":")[1])
-
-        $ElapsedSession = $EndTime - $StartTime
-        $CumulativeTime += $ElapsedSession
     }
 
     if ($ReturnDatetimeObject -eq $false) {
@@ -97,26 +104,27 @@ function Process-DailyNote {
 
         [ValidateScript({Test-Path $_, "Daily note not found at $_"})]
         [String]
-        $DailyNotePath = "$env:git\obsidian-vaults\notey-notes\daily notes\$($Date | Get-Date -Format yyyy-MM-dd) daily note.md"
+        $DailyNotePath = "$env:git\obsidian-vaults\notey-notes\daily notes\$($Date | Get-Date -Format yyyy-MM-dd) daily note.md",
+
+        [switch]
+        $Experiment
     )
+    $ClockFormatPattern = "\d{1,2}:\d{1,2}\s?-\s?\d{1,2}:\d{1,2}"
+    $DurationFormatPattern = "\d{1,}\s?h([a-z\s]*\d{1,}\s?m[a-z]*)?"
 
     # Import today's daily note automagically instead of piping in the data
-    if (!(Test-Path -Path "$env:git\obsidian-vaults\notey-notes\")) {
-        return "Obsidian vault not found."
-    }
     $DailyNoteContent = Get-Content $DailyNotePath
 
     # Only include lines with a checkbox
     $DailyNoteContent = $DailyNoteContent | Where-Object {($_ -match "-\s\[\s\]") -or ($_ -match "-\s\[x\]")}
     foreach ($Line in $DailyNoteContent) {
         $WorkSessionExists = $false
-        # Get the raw sessions from each line and pass to Calculate-TimeElapsed
-        $Sections = $Line.Trim().Split(" ")
-        $RawSessions = ""
-        foreach ($Section in $Sections) {
+
+        if ($Experiment) {
+            # Determine if work session(s) is/are present
             # Does line have at least one session in "hh:mm-hh:mm" format or "h hour m minute" format
-            $ClockFormat = $Section -match "\d{1,2}:\d{1,2}\s?-\s?\d{1,2}:\d{1,2}"
-            $DurationFormat = $Section -match "\d{1,}\s?h([a-z\s]*\d{1,}\s?m[a-z]*)?"
+            $ClockFormat = $Line -match $ClockFormatPattern
+            $DurationFormat = $Line -match $DurationFormatPattern
             # ^ Test data set for regex
             <#
             1h
@@ -128,11 +136,51 @@ function Process-DailyNote {
             1 hr 35 min
             1 hour 35 minutes
             #>
+            # If exists, split it into two (2) parts: description and time
             $WorkSessionExists = $ClockFormat -or $DurationFormat
-            #Write-Host "Section: $($Section)`nMatches: $($Matches)"
             if ($WorkSessionExists) {
-                $RawSessions = "$RawSessions$Section"
-                #Write-Host "$Section -> $RawSessions"
+                $Sections = $Line.Trim().Split(" ")
+                $RawSessions = ""
+                $Description = ""
+                #$DescriptionComplete = $false
+                foreach ($Section in $Sections) {
+                    if (($Section -notmatch $ClockFormatPattern) -and ($Section -notmatch $DurationFormatPattern)) {
+                        # Then it's part of the description
+                        $Description = "$Description$Section"
+                    } else {
+                        #$DescriptionComplete = $true
+                        # It's a type of work session
+                        # So re-assemble work sessions and send off to other functions
+                        $RawSessions = "$RawSessions$Section"
+                    }
+                }
+            }
+        } else {
+            
+            # Get the raw sessions from each line and pass to Calculate-TimeElapsed
+            $Sections = $Line.Trim().Split(" ")
+            $RawSessions = ""
+            foreach ($Section in $Sections) {
+                # Does line have at least one session in "hh:mm-hh:mm" format or "h hour m minute" format
+                $ClockFormat = $Section -match $ClockFormatPattern
+                $DurationFormat = $Section -match $DurationFormatPattern
+                # ^ Test data set for regex
+                <#
+                1h
+                1hour
+                1 hour
+                1 h 35 m
+                1h35m
+                1hr35min
+                1 hr 35 min
+                1 hour 35 minutes
+                #>
+                $WorkSessionExists = $ClockFormat -or $DurationFormat
+                #Write-Host "Section: $($Section)`nMatches: $($Matches)"
+                if ($WorkSessionExists) {
+                    $RawSessions = "$RawSessions$Section"
+                    #Write-Host "$Section -> $RawSessions"
+                }
             }
         }
         if (($null -ne $RawSessions) -and ($RawSessions -ne "")) {
@@ -151,5 +199,6 @@ function Process-DailyNote {
             Write-Host "$Line --> " -NoNewline
             Write-Host "$($ElapsedTime.Hours) hours $($ElapsedTime.Minutes) minutes" -ForegroundColor $ForegroundColor
         }
+        
     }
 }

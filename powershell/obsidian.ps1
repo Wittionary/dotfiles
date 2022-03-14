171a405697514b1b801d14f5fb433fd8880422b0
Witt Allen <Wittionary@users.noreply.github.com>
2022-03-14 15:55:36 -0500

    Refactoring Process-DailyNote

--- powershell/obsidian.ps1
+++ powershell/obsidian.ps1
@@ -104,10 +104,7 @@ function Process-DailyNote {
 
         [ValidateScript({Test-Path $_, "Daily note not found at $_"})]
         [String]
-        $DailyNotePath = "$env:git\obsidian-vaults\notey-notes\daily notes\$($Date | Get-Date -Format yyyy-MM-dd) daily note.md",
-
-        [switch]
-        $Experiment
+        $DailyNotePath = "$env:git\obsidian-vaults\notey-notes\daily notes\$($Date | Get-Date -Format yyyy-MM-dd) daily note.md"
     )
     $ClockFormatPattern = "\d{1,2}:\d{1,2}\s?-\s?\d{1,2}:\d{1,2}"
     $DurationFormatPattern = "\d{1,}\s?h([a-z\s]*\d{1,}\s?m[a-z]*)?"
@@ -119,12 +116,14 @@ function Process-DailyNote {
     $DailyNoteContent = $DailyNoteContent | Where-Object {($_ -match "-\s\[\s\]") -or ($_ -match "-\s\[x\]")}
     foreach ($Line in $DailyNoteContent) {
         $WorkSessionExists = $false
-
-        if ($Experiment) {
-            # Determine if work session(s) is/are present
+            
+        # Get the raw sessions from each line and pass to Calculate-TimeElapsed
+        $Sections = $Line.Trim().Split(" ")
+        $RawSessions = ""
+        foreach ($Section in $Sections) {
             # Does line have at least one session in "hh:mm-hh:mm" format or "h hour m minute" format
-            $ClockFormat = $Line -match $ClockFormatPattern
-            $DurationFormat = $Line -match $DurationFormatPattern
+            $ClockFormat = $Section -match $ClockFormatPattern
+            $DurationFormat = $Section -match $DurationFormatPattern
             # ^ Test data set for regex
             <#
             1h
@@ -136,63 +135,26 @@ function Process-DailyNote {
             1 hr 35 min
             1 hour 35 minutes
             #>
-            # If exists, split it into two (2) parts: description and time
             $WorkSessionExists = $ClockFormat -or $DurationFormat
+            #Write-Host "Section: $($Section)`nMatches: $($Matches)"
             if ($WorkSessionExists) {
-                $Sections = $Line.Trim().Split(" ")
-                $RawSessions = ""
-                $Description = ""
-                #$DescriptionComplete = $false
-                foreach ($Section in $Sections) {
-                    if (($Section -notmatch $ClockFormatPattern) -and ($Section -notmatch $DurationFormatPattern)) {
-                        # Then it's part of the description
-                        $Description = "$Description$Section"
-                    } else {
-                        #$DescriptionComplete = $true
-                        # It's a type of work session
-                        # So re-assemble work sessions and send off to other functions
-                        $RawSessions = "$RawSessions$Section"
-                    }
-                }
-            }
-        } else {
-            
-            # Get the raw sessions from each line and pass to Calculate-TimeElapsed
-            $Sections = $Line.Trim().Split(" ")
-            $RawSessions = ""
-            foreach ($Section in $Sections) {
-                # Does line have at least one session in "hh:mm-hh:mm" format or "h hour m minute" format
-                $ClockFormat = $Section -match $ClockFormatPattern
-                $DurationFormat = $Section -match $DurationFormatPattern
-                # ^ Test data set for regex
-                <#
-                1h
-                1hour
-                1 hour
-                1 h 35 m
-                1h35m
-                1hr35min
-                1 hr 35 min
-                1 hour 35 minutes
-                #>
-                $WorkSessionExists = $ClockFormat -or $DurationFormat
-                #Write-Host "Section: $($Section)`nMatches: $($Matches)"
-                if ($WorkSessionExists) {
-                    $RawSessions = "$RawSessions$Section"
-                    #Write-Host "$Section -> $RawSessions"
-                }
+                $RawSessions = "$RawSessions$Section"
+                #Write-Host "$Section -> $RawSessions"
             }
         }
+
         if (($null -ne $RawSessions) -and ($RawSessions -ne "")) {
             $ElapsedTime = Calculate-TimeElapsed -RawSessions $RawSessions -ReturnDatetimeObject $true
             $ForegroundColor = "White"
 
             # If a time has already been registered as entered, add a visual indicator that's the case
             if ($Line -match "-\s\[\s\]") {
-                $Line = $Line.Replace("- [ ] ","")
+                #$Line = $Line.Replace("- [ ] ","")
+                $Line = $Description
                 $ForegroundColor = "Green"
             } elseif ($Line -match "-\s\[x\]") {
-                $Line = $Line.Replace("- [x] ","")
+                #$Line = $Line.Replace("- [x] ","")
+                $Line = $Description
                 $ForegroundColor = "Yellow"
             }
 
@@ -201,4 +163,63 @@ function Process-DailyNote {
         }
         
     }
+}
+
+function Process-DailyNote2 {
+    param (
+        [datetime]
+        $Date = $(Get-Date),
+
+        [ValidateScript({Test-Path $_, "Daily note not found at $_"})]
+        [String]
+        $DailyNotePath = "$env:git\obsidian-vaults\notey-notes\daily notes\$($Date | Get-Date -Format yyyy-MM-dd) daily note.md"
+    )
+    $ClockFormatPattern = "\d{1,2}:\d{1,2}\s?-\s?\d{1,2}:\d{1,2}"
+    $DurationFormatPattern = "\d{1,}\s?h([a-z\s]*\d{1,}\s?m[a-z]*)?"
+    $TaskIncompletePattern = "-\s\[\s\]"
+    $TaskCompletePattern = "-\s\[x\]"
+
+    # Import today's daily note automagically instead of piping in the data
+    $DailyNoteContent = Get-Content $DailyNotePath
+
+    # Only include lines with a time entry
+    $DailyNoteContent = $DailyNoteContent | Where-Object {($_ -match $ClockFormatPattern) -or ($_ -match $DurationFormatPattern)}
+
+    # Need to create one task object per line so it's easier to process via API later
+    # Properties:
+    # - title/description
+    # - time entries (or at least one, big time entry that's been added up)
+    # - completed yet (x or no x)
+
+    $Tasks = @()
+    foreach ($Line in $DailyNoteContent) {
+        $Task = @{}
+        # Get task status
+        if ($Line -match $TaskIncompletePattern) {
+            $Task.Status = "incomplete"
+        } elseif ($Line -match $TaskCompletePattern) {
+            $Task.Status = "complete"
+        }
+        Write-Host "$($Task.Status)"
+
+        # Remove that part from line
+        # zzz abcdefg 1234 -> abcdefg 1234
+        $Line = $Line.Substring(($Matches.0).Length + 1, $Line.Length - ($Matches.0).Length - 1).Trim()
+
+        # Get the time entries section
+        if ($Line -match $ClockFormatPattern -or $Line -match $DurationFormatPattern) {
+            Write-Host "$Line"
+            $Sessions = $Line.Substring($Line.IndexOf($Matches.0) + 1, ($Line.Length - $Line.IndexOf($Matches.0))).Trim()
+            Write-Host "$Sessions"
+            $Task.RawSessions = $Sessions
+            
+            # Get the title/description section
+            $Task.Title = $Line.Substring(0, $Line.IndexOf($Matches.0) + 1)
+        }
+
+
+        $Tasks += $Task
+    }
+    
+    Write-Host "$($Tasks)"
 }

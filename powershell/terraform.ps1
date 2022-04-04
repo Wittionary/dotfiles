9a7f8a07b79dd7b02270ad57b27a48b83b895724
Witt Allen <Wittionary@users.noreply.github.com>
2022-04-04 11:54:48 -0500

    Added alias for terragrunt

--- powershell/terraform.ps1
+++ powershell/terraform.ps1
@@ -1,4 +1,6 @@
 # Terraform alias
 New-Alias -Name "tf" -Value "terraform.exe" -Description "Saves on 'terraform' keystrokes" -ErrorAction SilentlyContinue
 
+# Terragrunt alias
+New-Alias -Name "tg" -Value "terragrunt.exe" -Description "Saves on 'terragrunt' keystrokes" -ErrorAction SilentlyContinue
 # May add shortcuts (like in git.ps1) later: e.g. tf pa (plan apply)

c33dc3ef0588bb43ffff9e925c22852ca10731d8
Witt Allen <wittionary@users.noreply.github.com>
2022-05-15 20:17:19 -0500

    Removed $Toast. Istill like the idea of a motd or wotd.

--- powershell/Profile.ps1
+++ powershell/Profile.ps1
@@ -8,17 +8,6 @@ See this resource for details:
 https://devblogs.microsoft.com/scripting/understanding-the-six-powershell-profiles/
 #>
 Clear-Host
-# https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=Basic&text=dotfiles
-# DOTFILES
-$Toast = "
-d8888b.  .d88b.  d888888b d88888b d888888b db      d88888b .d8888. 
-88  ``8D .8P  Y8. ``~~88~~`` 88'       ``88'   88      88'     88'  YP 
-88   88 88    88    88    88ooo      88    88      88ooooo ``8bo.   
-88   88 88    88    88    88~~~      88    88      88~~~~~   ``Y8b.    
-88  .8D ``8b  d8'    88    88        .88.   88booo. 88.     db   8D  
-Y8888D'  ``Y88P'     YP    YP      Y888888P Y88888P Y88888P ``8888Y'                                                                    
-"
-#Write-Host $Toast
 
 try {
     $Functions = Get-ChildItem -Path "$ENV:git\dotfiles\powershell\*.ps1" | Where-Object {$_.Name -ne "profile.ps1"}

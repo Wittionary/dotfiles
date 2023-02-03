d2e6808899c79494a7cbe7a42736463051a788c5
Witt Allen <Wittionary@users.noreply.github.com>
2023-02-03 15:56:54 -0600

    Refactored Process-DailyNote to prep for making time entries

--- powershell/obsidian.ps1
+++ powershell/obsidian.ps1
@@ -97,76 +97,6 @@ function Calculate-TimeDuration {
 }
 
 # Turn recorded daily note work sessions into sums of time per task/project
-function Process-DailyNoteLEGACY {
-    param (
-        [datetime]
-        $Date = $(Get-Date),
-
-        [ValidateScript({Test-Path $_, "Daily note not found at $_"})]
-        [String]
-        $DailyNotePath = "$env:git\obsidian-vaults\notey-notes\daily notes\$($Date | Get-Date -Format yyyy-MM-dd) daily note.md"
-    )
-    $ClockFormatPattern = "\d{1,2}:\d{1,2}\s?-\s?\d{1,2}:\d{1,2}"
-    $DurationFormatPattern = "\d{1,}\s?[hoursminute]+(\d{1,}\s?m[inutes]+)?"
-
-    # Import today's daily note automagically instead of piping in the data
-    $DailyNoteContent = Get-Content $DailyNotePath
-
-    # Only include lines with a checkbox
-    $DailyNoteContent = $DailyNoteContent | Where-Object {($_ -match "-\s\[\s\]") -or ($_ -match "-\s\[x\]")}
-    foreach ($Line in $DailyNoteContent) {
-        $WorkSessionExists = $false
-            
-        # Get the raw sessions from each line and pass to Calculate-TimeElapsed
-        $Sections = $Line.Trim().Split(" ")
-        $RawSessions = ""
-        foreach ($Section in $Sections) {
-            # Does line have at least one session in "hh:mm-hh:mm" format or "h hour m minute" format
-            $ClockFormat = $Section -match $ClockFormatPattern
-            $DurationFormat = $Section -match $DurationFormatPattern
-            # ^ Test data set for regex
-            <#
-            30m
-            30minutes
-            30 m
-            30 minutes
-            1h
-            1hour
-            1 hour
-            1 h 35 m
-            1h35m
-            1hr35min
-            1 hr 35 min
-            1 hour 35 minutes
-            #>
-            $WorkSessionExists = $ClockFormat -or $DurationFormat
-            #Write-Host "Section: $($Section)`nMatches: $($Matches)"
-            if ($WorkSessionExists) {
-                $RawSessions = "$RawSessions$Section"
-                #Write-Host "$Section -> $RawSessions"
-            }
-        }
-
-        if (($null -ne $RawSessions) -and ($RawSessions -ne "")) {
-            $ElapsedTime = Calculate-TimeElapsed -RawSessions $RawSessions -ReturnDatetimeObject $true
-            $ForegroundColor = "White"
-
-            # If a time has already been registered as entered, add a visual indicator that's the case
-            if ($Line -match "-\s\[\s\]") {
-                $Line = $Line.Replace("- [ ] ","")
-                $ForegroundColor = "Green"
-            } elseif ($Line -match "-\s\[x\]") {
-                $Line = $Line.Replace("- [x] ","")
-                $ForegroundColor = "Yellow"
-            }
-
-            Write-Host "$Line --> " -NoNewline
-            Write-Host "$($ElapsedTime.Hours) hours $($ElapsedTime.Minutes) minutes" -ForegroundColor $ForegroundColor
-        }
-        
-    }
-}
-
 function Process-DailyNote {
     param (
         [datetime]
@@ -190,15 +120,55 @@ function Process-DailyNote {
     # Only include lines with a time entry
     $DailyNoteContent = $DailyNoteContent | Where-Object {($_ -match $ClockFormatPattern) -or ($_ -match $DurationFormatPattern)}
 
-    # Need to create one task object per line so it's easier to process via API later
-    # Properties:
-    # - title/description
-    # - time entries (or at least one, big time entry that's been added up)
-    # - completed yet (x or no x)
-
     $Tasks = @()
     foreach ($Line in $DailyNoteContent) {
-        $Task = @{}
+        $Task = Parse-DailyNoteLine -Line $Line
+        $Tasks += $Task
+    }
+    
+    # Output to user
+    Write-Host "`nThere are " -NoNewLine
+    Write-Host "$($Tasks.Length)" -ForegroundColor Cyan -NoNewline
+    Write-Host " tasks for $($Date | Get-Date -Format dddd), $($Date | Get-Date -Format 'MMMM dd')"
+    foreach ($Task in $Tasks) {
+        Display-DailyNoteTask -Task $Task
+    }
+    
+}
+
+function Get-AcceloTicketOptions{
+    param (
+        [Parameter(
+            Mandatory=$true,
+            Position=0
+        )]
+        [string]
+        $Description
+    )
+
+    switch -Regex ($Description) {
+        "training" { return "training"; break} # Internal - Professional Dev/Training
+        "webinar" { return "training"; break}
+        "meeting" { return "internal meeting"; break} # Internal Meetings
+        "do not bill" { return "internal projects"; break} # Internal Projects
+        "dnb" { return "internal projects"; break}
+        "DNB" { return "internal projects"; break}
+        # Internal - PreSales
+        Default {return "unknown"}
+    }
+}
+
+function Parse-DailyNoteLine{
+    param (
+        [Parameter(
+            Mandatory=$true,
+            Position=0
+        )]
+        [string]
+        $Line
+    )
+
+    $Task = @{}
         # Get task status
         if ($Line -match $TaskIncompletePattern) {
             $Task.Status = "incomplete"
@@ -239,42 +209,26 @@ function Process-DailyNote {
         # Determine which Accelo ticket it might go towards
         $Task.AcceloTicket = Get-AcceloTicketOptions -Description "$($Task.Title) $($Task.Notes)"
         
-        $Tasks += $Task
-    }
-    
-    # Output to user
-    Write-Host "`nThere are $($Tasks.Length) tasks for $($Date | Get-Date -Format dddd), $($Date | Get-Date -Format 'MMMM dd')"
-    foreach ($Task in $Tasks) {
-        if ($Task.Status -eq "complete") {
-            $ForegroundColor = "Green"
-        } elseif ($Task.Status -eq "incomplete") {
-            $ForegroundColor = "Yellow"
-        }
         
-        Write-Host "$($Task.Title) --> " -NoNewline
-        Write-Host "$($Task.CumulativeTime.Hours) hours $($Task.CumulativeTime.Minutes) minutes" -ForegroundColor $ForegroundColor -NoNewline
-        Write-Host "    (TICKET: $($Task.AcceloTicket); NOTES: $($Task.Notes))"
-    }
+        return $Task
 }
 
-function Get-AcceloTicketOptions{
+function Display-DailyNoteTask{
     param (
         [Parameter(
             Mandatory=$true,
             Position=0
         )]
-        [string]
-        $Description
+        $Task
     )
 
-    switch -Regex ($Description) {
-        "training" { return "training"; break} # Internal - Professional Dev/Training
-        "webinar" { return "training"; break}
-        "meeting" { return "internal meeting"; break} # Internal Meetings
-        "do not bill" { return "internal projects"; break} # Internal Projects
-        "dnb" { return "internal projects"; break}
-        "DNB" { return "internal projects"; break}
-        # Internal - PreSales
-        Default {return "unknown"}
+    if ($Task.Status -eq "complete") {
+        $ForegroundColor = "Green"
+    } elseif ($Task.Status -eq "incomplete") {
+        $ForegroundColor = "Yellow"
     }
+    
+    Write-Host "$($Task.Title) --> " -NoNewline
+    Write-Host "$($Task.CumulativeTime.Hours) hours $($Task.CumulativeTime.Minutes) minutes" -ForegroundColor $ForegroundColor -NoNewline
+    Write-Host "    (TICKET: $($Task.AcceloTicket); NOTES: $($Task.Notes))"
 }

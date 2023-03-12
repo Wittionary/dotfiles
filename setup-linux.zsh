f256d5136eeebebff2d207775734be4c53f2bb58
Witt Allen <wittionary@users.noreply.github.com>
2023-03-12 18:16:48 -0500

    Added more reliable output to user

--- setup-linux.zsh
+++ setup-linux.zsh
@@ -31,8 +31,7 @@ if [ "$repo_exists" = true ]; then
     elif [[ -L ~/.zshrc ]]; then
         echo "~/.zshrc is already symlinked"
     else
-        ln -s -v $GIT_PATH/dotfiles/zsh/.zshrc ~/.zshrc
-        echo "~/.zshrc is now a symlink"
+        ln -s -v $GIT_PATH/dotfiles/zsh/.zshrc ~/.zshrc && echo "~/.zshrc is now a symlink"
     fi
 
     # .zshenv
@@ -41,8 +40,7 @@ if [ "$repo_exists" = true ]; then
     elif [[ -L ~/.zshenv ]]; then
         echo "~/.zshenv is already symlinked"
     else
-        ln -s $GIT_PATH/dotfiles/zsh/.zshenv ~/.zshenv
-        echo "~/.zshenv is now a symlink"
+        ln -s $GIT_PATH/dotfiles/zsh/.zshenv ~/.zshenv && echo "~/.zshenv is now a symlink"
     fi
 
     # VIM ---------------------------------------------------------
@@ -52,22 +50,18 @@ if [ "$repo_exists" = true ]; then
     elif [[ -L ~/.vimrc ]]; then
         echo "~/.vimrc is already symlinked"
     else
-        ln -s -v $GIT_PATH/dotfiles/vim/.vimrc ~/.vimrc
-        echo "~/.vimrc is now a symlink"
+        ln -s -v $GIT_PATH/dotfiles/vim/.vimrc ~/.vimrc && echo "~/.vimrc is now a symlink"
     fi
 
     # vim theme
     if [[ ! -d ~/.vim/colors ]]; then
-        mkdir -p ~/.vim/colors
-        echo "~/.vim/colors directory made"
+        mkdir -p ~/.vim/colors && echo "~/.vim/colors directory made"
     fi
     if [[ -f ~/.vim/colors/codedark.vim && ! ( -L ~/.vim/colors/codedark.vim ) ]]; then
         rm -v ~/.vim/colors/codedark.vim
-    elif [[ -L ~/.vim/colors/codedark.vim ]]; then
-        echo "~/.vim/colors/codedark.vim is already symlinked"
+    elif [[ -L ~/.vim/colors/codedark.vim ]]; then && echo "~/.vim/colors/codedark.vim is already symlinked"
     else
-        ln -s -v $GIT_PATH/dotfiles/vim/colors/codedark.vim ~/.vim/colors/codedark.vim
-        echo "~/.vim/colors/codedark.vim is now a symlink"
+        ln -s -v $GIT_PATH/dotfiles/vim/colors/codedark.vim ~/.vim/colors/codedark.vim && echo "~/.vim/colors/codedark.vim is now a symlink"
     fi
 
     # WSL ---------------------------------------------------------
@@ -77,9 +71,7 @@ if [ "$repo_exists" = true ]; then
     elif [[ -L /etc/wsl.conf ]]; then
         echo "/etc/wsl.conf is already symlinked"
     else
-        ln -s -v $GIT_PATH/dotfiles/wsl/ubuntu-wsl.conf /etc/wsl.conf
-        echo "/etc/wsl.conf is now a symlink"
-        ln -s -v $GIT_PATH/dotfiles/wsl/resolv.conf /etc/resolv.conf
-        echo "/etc/resolv.conf is now a symlink"
+        ln -s -v $GIT_PATH/dotfiles/wsl/ubuntu-wsl.conf /etc/wsl.conf && echo "/etc/wsl.conf is now a symlink"
+        ln -s -v $GIT_PATH/dotfiles/wsl/resolv.conf /etc/resolv.conf && echo "/etc/resolv.conf is now a symlink"
     fi
 fi

58ca9a07f19621c892369af0b2696135f452af4d
Witt Allen <Wittionary@users.noreply.github.com>
2024-03-11 16:00:39 -0500

    Added 'which' alias

---
+++ powershell/which.ps1
@@ -0,0 +1,7 @@
+Set-Alias -Name which -Value Get-Command
+# Example: 
+# > which az
+# 
+# CommandType     Name          Version    Source
+# -----------     ----          -------    ------
+# Application     az.cmd        0.0.0.0    C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd

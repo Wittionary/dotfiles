1af4dc4416c1e0f1c548e3e85be1ba727353fcf9
Witt Allen <wittionary@users.noreply.github.com>
2024-12-06 14:33:53 -0600

    Added support for checking out a specified branch

--- powershell/git.ps1
+++ powershell/git.ps1
@@ -43,14 +43,14 @@ function g {
         # Checkout
         if ($String -ne "") {
             git checkout $String
+        } else {
+            # TODO: test for existance of fzf
+            & git checkout $(
+                    git branch --list | 
+                    Where-Object {$_ -notmatch "\*"} | # everything but the currently selected branch
+                    ForEach-Object {$_.trim()} | fzf --height 25% --layout=reverse
+                )
         }
-        
-        # TODO: test for existance of fzf
-        & git checkout $(
-                git branch --list | 
-                Where-Object {$_ -notmatch "\*"} | # everything but the currently selected branch
-                ForEach-Object {$_.trim()} | fzf --height 25% --layout=reverse
-            )
     }
 }
 

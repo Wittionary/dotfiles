06599036952ff8fed9bc9fe954afd63bd97094b8
Witt Allen <wittionary@users.noreply.github.com>
2025-07-15 08:47:11 -0500

    Added git push.autoSetupRemote; set to true

--- git/_setup.ps1
+++ git/_setup.ps1
@@ -6,7 +6,13 @@ if ($(git config --global user.email) -ne $GithubPrivateEmail) {
     git config --global user.email $GithubPrivateEmail
     Write-Host "git config --global user.email -> $(git config --global user.email)"
 }
+
 if ($(git config --global user.name) -ne $Name) {
     git config --global user.name $Name
     Write-Host "git config --global user.name -> $(git config --global user.name)"
+}
+
+if ($(git config --global push.autoSetupRemote) -ne "true") {
+    git config --global push.autoSetupRemote "true"
+    Write-Host "git config --global push.autoSetupRemote -> $(git config --global push.autoSetupRemote)"
 }
